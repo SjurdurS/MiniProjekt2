89e939ff53129bccb670a53699801ded11e316d5
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-03 13:44:26 +0200

    fark

--- src/miniprojekt2/PSS_Server.java
+++ src/miniprojekt2/PSS_Server.java
@@ -1,12 +1,13 @@
-package miniprojekt2;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * *
@@ -16,28 +17,31 @@ import java.util.ArrayList;
  */
 public class PSS_Server {
 
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        InetAddress hostname = InetAddress.getLocalHost();
+    private static ArrayList<Socket> sinks = new ArrayList<Socket>();
+
+    public PSS_Server() throws UnknownHostException, IOException {
         int sourcePort = 8;
         int sinkPort = 9;
 
-        new SourceServer(hostname, sourcePort);
-        new SinkServer(hostname, sinkPort);
+        new SourceServer(sourcePort);
+        new SinkServer(sinkPort);
     }
 
-    static class SourceServer implements Runnable {
+    static class SourceServer extends Thread {
+
         ServerSocket ss;
         Socket client = null;
 
-        public SourceServer(InetAddress hostname, int sourcePort) throws IOException {
+        public SourceServer(int sourcePort) throws IOException {
             this.ss = new ServerSocket(sourcePort);
+            this.start();
         }
 
         public void run() {
             while (true) {
                 try {
                     client = ss.accept();
-                    System.out.println("Connected to client : " + client.getInetAddress().getHostName());
+                    System.out.println("Connected to Source : " + client.getInetAddress().getHostName());
                     new SourceThread(client);
 
                 } catch (Exception e) {
@@ -47,23 +51,24 @@ public class PSS_Server {
         }
     }
 
-    static class SinkServer implements Runnable {
-        ArrayList<Thread> sinks = new ArrayList<>();
+    static class SinkServer extends Thread {
+
         ServerSocket ss;
         Socket client = null;
 
-        public SinkServer(InetAddress hostname, int sinkPort) throws IOException {
+        public SinkServer(int sinkPort) throws IOException {
             this.ss = new ServerSocket(sinkPort);
-            
+            this.start();
         }
 
         public void run() {
             while (true) {
                 try {
                     client = ss.accept();
-                    System.out.println("Connected to client : " + client.getInetAddress().getHostName());
-                    sinks.add(new SinkThread(client));
-                    
+                    System.out.println("Connected to Sink : " + client.getInetAddress().getHostName());
+                    sinks.add(client);
+                    System.out.println(sinks.size());
+
                 } catch (Exception e) {
                     System.err.println(e.getMessage());
                 }
@@ -71,50 +76,69 @@ public class PSS_Server {
         }
     }
 
-
     static class SourceThread extends Thread {
 
-        SourceThread(Socket sourceSocket) {
+        Socket sourceSocket = null;
 
+        SourceThread(Socket sourceSocket) {
+            this.sourceSocket = sourceSocket;
+            this.start();
         }
 
+        public void run() {
+            try {
+                System.out.println("Connected to client : " + sourceSocket.getInetAddress().getHostName());
+
+                try (InputStream is = sourceSocket.getInputStream();) {
+                    byte[] buf = new byte[1024];
+                    int eof;
+                    do {
+                        eof = is.read(buf);
+                        if (eof > 0) {
+                            Iterator<Socket> sinksIter = sinks.iterator();
+                            while (sinksIter.hasNext()) {
+                                Socket sink = sinksIter.next();
+                                try {
+                                    OutputStream out = sink.getOutputStream();
+                                    out.write(buf, 0, eof);
+                                } catch (IOException e) {
+                                    System.err.println(e.getMessage());                                    
+                                    try {
+                                        System.out.println("Removing socket from list");
+                                        sinksIter.remove();
+                                    } catch (Exception ex1) {
+                                        System.err.println("Remove error: " + ex1);
+                                    }
+                                    try {
+                                        System.out.println("Closing socked");
+                                        sink.close();
+                                        
+                                    } catch (IOException ex) {
+                                        System.err.println("Closing error: " + e);
+                                    }
+                                }
+
+                            }
+                        }
+                    } while (eof >= 0);
+
+                } catch (IOException ex) {
+                    System.err.println("Connection died:" + ex.getMessage());
+                }
+
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 
-    static class SinkThread extends Thread {
+    public static void main(String[] args) {
+        try {
+            new PSS_Server();
 
-        SinkThread(Socket sinkSocket) {
-
+        } catch (IOException ex) {
+            Logger.getLogger(PSS_Server.class
+                    .getName()).log(Level.SEVERE, null, ex);
         }
-        
-        public void sendMessage(OutputStream out){
-            
-        }
-
     }
 }
-/**
-    public static void main(String args[]) {
-        try {
-            ServerSocket sourceserver
-                    = new ServerSocket(8);
-            while (true) {
-                Socket p = server.accept();
-                new Thread(new ServerThread(p)).start();
-            }
-        } catch (Exception ex) {
-            System.err.println("Error : " + ex.getMessage());
-        }
-
-        try {
-            ServerSocket sinkserver = new ServerSocket(9);
-            while (true) {
-                Socket p
-                        = server.accept();
-                arraylist.add(new Thread(new ServerThread(p)).start());
-            }
-        } catch (Exception ex) {
-            System.err.println("Error : " + ex.getMessage());
-        }
-    }
-
-}**/
